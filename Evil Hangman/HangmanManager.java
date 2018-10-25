24d10caa3b0893138ea7e77062b2e3244623fbd6
Issac Ye <jy98@uw.edu>
2018-10-24 17:58:48 -0700

    file change due to different java versions

--- Evil Hangman/HangmanManager.java
+++ Evil Hangman/HangmanManager.java
@@ -16,12 +16,16 @@ public class HangmanManager {
    
    private Map<String, Set<String>> wordFamily;
    
+   private int length;
+   
+   private String pattern;
+   
 
    public HangmanManager(Collection<String> dictionary, int length, int max) {
       if (length < 1 || max < 0) {
          throw new IllegalArgumentException();
       }
-      
+      this.length = length;
       guesses = max;
       wrongGuesses = max;
       wordSet = new TreeSet<String>();
@@ -30,6 +34,11 @@ public class HangmanManager {
          if (word.length() == length) {
             wordSet.add(word);
          }
+      }
+      
+      pattern = "-";
+      for (int i = 0; i < length - 1; i++) {
+         pattern += " -";
       }
    }   
    
@@ -57,22 +66,73 @@ public class HangmanManager {
       if (wordSet.isEmpty()) {
          throw new IllegalStateException();
       }
-
-      String maxlength = "";
-      int maxlen = 0;
-      for (String key: wordFamily.keySet()) {
-         if (wordFamily.get(key).size() > maxlen) {
-            maxlen = wordFamily.get(key).size();
-            maxlength = key;
-         }
-      }
-      return maxlength;
+      
+      return pattern;
    }
    
    
  
  
    
+//    public int record(char guess) {
+//       if (guesses < 1 || wordSet.isEmpty()) {
+//          throw new IllegalStateException();
+//       } else if (!wordSet.isEmpty() && guessedChar.contains(guess)) {
+//          throw new IllegalArgumentException();
+//       }
+//       
+//       guessedChar.add(guess);
+//       wordFamily = new TreeMap<String, Set<String>>();
+//       for (String word: wordSet) {
+//          String temp = "";
+//          for (int i = 0; i < word.length(); i++) {
+//             if (word.substring(i, i + 1).equals(guess + "")) {
+//                temp += word.substring(i, i + 1);
+//                if (i != word.length() - 1) {
+//                   temp += " ";
+//                }
+//             } else if(i != word.length() - 1){
+//                temp += "- ";
+//             } else {
+//                temp += "-";
+//             }
+//          }
+//          if (!wordFamily.containsKey(temp)) {
+//             wordFamily.put(temp, new TreeSet<String>());
+//          }
+//          wordFamily.get(temp).add(word);        
+//       }
+//       
+//       
+//       String maxlength = "";
+//       int maxlen = 0;
+//       for (String key: wordFamily.keySet()) {
+//          if (wordFamily.get(key).size() > maxlen) {
+//             maxlen = wordFamily.get(key).size();
+//             maxlength = key;
+//          }
+//       }
+//       
+//       int count = 0;
+//       for (int i = 0; i < maxlength.length(); i++) {
+//          if (maxlength.substring(i, i + 1).equals(guess + "")){
+//             count++;
+//          }
+//       }
+//       
+//       if (count == 0) {
+//          wrongGuesses--;
+//       }
+//       
+//       wordSet.clear();
+//       for (String word: wordFamily.get(maxlength)) {
+//          wordSet.add(word);   
+//       }
+//       System.out.println(wordFamily);
+//       return count;
+//    }
+
+
    public int record(char guess) {
       if (guesses < 1 || wordSet.isEmpty()) {
          throw new IllegalStateException();
@@ -80,7 +140,35 @@ public class HangmanManager {
          throw new IllegalArgumentException();
       }
       
+      int count = 0;
       guessedChar.add(guess);
+      
+      createMap(guess);
+      
+      int max = 0;
+      String patternKey = "";
+      for (String key: wordFamily.keySet()) {
+         if (wordFamily.get(key).size() > 0) {
+            max = wordFamily.get(key).size();
+            patternKey = key;
+         }
+      }
+      
+      wordSet = wordFamily.get(patternKey);
+      
+      for (int i = 0; i < patternKey.length(); i++) {
+         if (patternKey.substring(i, i + 1).equals(pattern.substring(i, i + 1)) || 
+            patternKey.substring(i, i + 1).equals("-")) {
+            pattern = pattern.substring(0, i) + patternKey.substring(i, i + 1) + pattern.substring(i + 1);
+            count++;
+         }
+      }
+      
+      return count;
+   }
+   
+   
+   private void createMap(char guess) {
       wordFamily = new TreeMap<String, Set<String>>();
       for (String word: wordSet) {
          String temp = "";
@@ -101,35 +189,12 @@ public class HangmanManager {
          }
          wordFamily.get(temp).add(word);        
       }
-      
-      
-      String maxlength = "";
-      int maxlen = 0;
-      for (String key: wordFamily.keySet()) {
-         if (wordFamily.get(key).size() > maxlen) {
-            maxlen = wordFamily.get(key).size();
-            maxlength = key;
-         }
-      }
-      
-      int count = 0;
-      for (int i = 0; i < maxlength.length(); i++) {
-         if (maxlength.substring(i, i + 1).equals(guess + "")){
-            count++;
-         }
-      }
-      
-      if (count == 0) {
-         wrongGuesses--;
-      }
-      
-      wordSet.clear();
-      for (String word: wordFamily.get(maxlength)) {
-         wordSet.add(word);   
-      }
-//       System.out.println(wordFamily);
-      return count;
    }
+   
+   
+   
+   
+   
    
    
    public static void main(String[] args){
