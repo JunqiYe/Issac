e1016d2e18edc23c0778db854c02cee063694bbc
Issac PC <jy98@uw.edu>
2018-10-25 01:25:47 -0800

    minor changes

--- Evil Hangman/HangmanManager.java
+++ Evil Hangman/HangmanManager.java
@@ -1,9 +1,10 @@
 // Junqi Ye
 // CSE 143 AM with Sam Lee
 // HW4 Evil Hangman
-// This class manages the Hangman game. It has details about the current
-// pattern client will see, the number of guesses left for the client, and feed
-// back about the letter they guessed.
+// This class manages the Hangman game. It manages details about all the 
+// possible words, the current word pattern, the number of guesses left, and 
+// feed back about the letter they guessed.
+
 import java.util.*;
 
 public class HangmanManager {
@@ -31,8 +32,8 @@ public class HangmanManager {
    // allowed to guess the word. If the length for the guessing word is less
    // than 1 or if the maximum trys allowed is less than 0, 
    // throw IllegalArgumentException.
-   // post: Store all the word in the dictionary with the length that client 
-   // is guessing, save the number of the trys user is allowed. 
+   // post: Store all the word that have the length client is guessing, save 
+   // the number of the trys user is allowed. 
    public HangmanManager(Collection<String> dictionary, int length, int max) {
       this();
       if (length < 1 || max < 0) {
@@ -86,10 +87,9 @@ public class HangmanManager {
    
    
    
-   // post: return a String represents the current pattern that the client will
-   // see. The pattern is wither an alphabet letter of a "-". The dash. If the
-   // set of word is empty, throw IllegalStateException.
-   // represent the letter at that location has not been revealed yet.
+   // post: return a string represents the current pattern that the client will
+   // see. The pattern is either an alphabet letter of a "-". If the set of 
+   // word is empty, throw IllegalStateException.
    public String pattern() {
       if (wordSet.isEmpty()) {
          throw new IllegalStateException();
@@ -99,10 +99,10 @@ public class HangmanManager {
    
    
  
-   // pre : takes a char and test it if the character is in the word. If the
-   // set of words  is empty or the number of guess is less than 1, throw 
-   // IllegalStateException. If the words is not empty and a character passed
-   // was guessed before, throw  IllegalArgumentException.
+   // pre : takes a char and test if the character is in the word. If the
+   // set of words is empty or the number of guess is less than 1, throw 
+   // IllegalStateException. If the set of words is not empty and a character
+   // passed was guessed before, throw IllegalArgumentException.
    // post: returns the number of occurances of the character in the word.
    public int record(char guess) {
       if (guesses < 1 || wordSet.isEmpty()) {
@@ -110,32 +110,26 @@ public class HangmanManager {
       } else if (!wordSet.isEmpty() && guessedChar.contains(guess)) {
          throw new IllegalArgumentException();
       }
-      
-      int count = 0;
+     
       guessedChar.add(guess);     
       createMap(guess);
-      
       String keyPattern = patternForSet();
-      
       wordSet = new TreeSet<String>(wordFamily.get(keyPattern));
-
       return updatePattern(keyPattern);
    }
    
    
    
-   // a helper method that put word into the maps according to their pattern
-   // from the guessed character.
-   // pre : takes a char that represent the character that is being tested.
-   // post: fill the Map with the pattern and the word associated with that
-   // pattern. 
+   // helper method 
+   // pre : takes a char that represent the character that was guessed.
+   // post: associate each word to a pattern. 
    private void createMap(char guess) {
       wordFamily = new TreeMap<String, Set<String>>();
       for (String word: wordSet) {
          String temp = pattern;
          for (int i = 0; i < word.length(); i++) {
             if (word.substring(i, i + 1).equals(guess + "")) {
-               temp = temp.substring(0, i * 2) + word.substring(i, i + 1) + temp.substring(i * 2 + 1);
+               temp = replaceChar(temp, word, i);
             }
          }
          if (!wordFamily.containsKey(temp)) {
@@ -146,10 +140,8 @@ public class HangmanManager {
    }
    
    
-   
-   // a helper method that find the pattern that have the most word in it.
-   // post: return the String that contains the pattern that have the most word
-   // in the pattern.
+   // helper method
+   // post: return the pattern that have the most word in it.
    private String patternForSet() {
       int max = 0;
       String keyPattern = "";
@@ -164,17 +156,18 @@ public class HangmanManager {
    
    
    
-   // a helper method that updates the pattern for the guessing word.
-   // pre : takes a String key pattern that has the most word following this 
-   // pattern.
+   // helper method 
+   // pre : takes a string representing the pattern that has the most words.
    // post: return the number of differences between the previous pattern and 
    // the new pattern. Update the number of guesses avaliable. Update the 
    // current pattern.
    private int updatePattern(String keyPattern) {
       int count = 0;
       for (int i = 0; i < keyPattern.length(); i += 2) {
-         if (!keyPattern.substring(i, i + 1).equals(pattern.substring(i, i + 1)) && pattern.substring(i, i + 1).equals("-")) {
-            pattern = pattern.substring(0, i) + keyPattern.substring(i, i + 1) + pattern.substring(i + 1);
+         if (!keyPattern.substring(i, i + 1).equals(pattern.substring(i, i + 1)) &&
+            pattern.substring(i, i + 1).equals("-")) {
+            
+            pattern = replaceChar(pattern, keyPattern, i);
             count++;
          }
       }
@@ -182,5 +175,16 @@ public class HangmanManager {
          guesses--;
       }
       return count;  
+   }
+   
+   
+   
+   // pre : takes two string and an int
+   // post: replaces the letter in first string at index with the letter in the 
+   // second string at the same index.
+   private String replaceChar(String s1, String s2, int index) {
+      return s1.substring(0, index) + 
+             s2.substring(index, index + 1) + 
+             s1.substring(index + 1);
    }
 }
