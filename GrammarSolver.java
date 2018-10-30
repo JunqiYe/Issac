6e804a2fec2965a15d97f663ffebe1f07a3f2e54
Issac PC <jy98@uw.edu>
2018-10-30 01:03:03 -0800

    writing codes

--- GrammarSolver.java
+++ GrammarSolver.java
@@ -3,24 +3,42 @@
 // HW5
 //
 
-public class GrammarSolver() {
+import java.util.*;
+
+public class GrammarSolver{
+   
+   private SortedMap<String, String> grammar;
    
    public GrammarSolver(List<String> grammar) {
    
+      if (grammar.size() == 0 ) {
+         throw new IllegalArgumentException();
+      }
+   
+      this.grammar = new TreeMap<String, String>();
+      
+      for (String string: grammar) {
+         String[] grammarArr = string.split("::=");
+         if (this.grammar.containsKey(grammarArr[0])) {
+            throw new IllegalArgumentException();
+         } else {
+            this.grammar.put(grammarArr[0],grammarArr[1]);
+         }
+      }
    }
    
    
-   public boolean grammarContains(String symbol) {
-   
-   }
-   
-   
-   public String[] generate(String symbol, int times) {
-   
-   }
-   
-   public String getSymbols() {
-   
-   }
+//    public boolean grammarContains(String symbol) {
+//       return true;
+//    }
+//    
+//    
+//    public String[] generate(String symbol, int times) {
+//       return null;
+//    }
+//    
+//    public String getSymbols() {
+//       return "";
+//    }
 
 }
