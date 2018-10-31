79c03ee9ff6a363c3152deb0ebc2d16d19f245ba
Issac PC <jy98@uw.edu>
2018-10-30 21:05:51 -0800

    general program

--- GrammarSolver.java
+++ GrammarSolver.java
@@ -19,26 +19,70 @@ public class GrammarSolver{
       
       for (String string: grammar) {
          String[] grammarArr = string.split("::=");
-         if (this.grammar.containsKey(grammarArr[0])) {
+         if (this.grammarContains(grammarArr[0])) {
             throw new IllegalArgumentException();
          } else {
-            this.grammar.put(grammarArr[0],grammarArr[1]);
+            this.grammar.put(grammarArr[0], grammarArr[1]);
          }
       }
    }
    
    
-//    public boolean grammarContains(String symbol) {
-//       return true;
-//    }
+   public boolean grammarContains(String symbol) { 
+      return grammar.containsKey(symbol);
+   }
+
+   
+   
+   public String[] generate(String symbol, int times) {
+      if (!grammarContains(symbol) || times < 0) {
+         throw new IllegalArgumentException();
+      }
+
+      String[] sentence = new String[times];
+      for (int i = 0; i < times; i++) {
+         sentence[i] = generate(symbol);
+      }
+      return sentence;
+   }
+   
+   
+   
+   private String generate(String symbol) {
+      String sentence = "";
+      String[] terminals = grammar.get(symbol).split("\\|");   
+      String[] individual = terminals[(int)(Math.random() * terminals.length)].split("\\s+");
+      
+      for (int i = 0; i < individual.length; i++) {
+         if (grammarContains(individual[i])) {
+            sentence += generate(individual[i]);
+         } else {
+            sentence += individual[i] + " ";
+         }
+      }
+      return sentence;
+   }
+
+
+   
+   public String getSymbols() {
+      String symbols = "[";
+      for (String key: grammar.keySet()) {
+         symbols += key;
+         symbols += ",";
+      }
+      symbols = symbols.substring(0,symbols.length() - 1) + "]";
+      return symbols; 
+   }
+   
 //    
-//    
-//    public String[] generate(String symbol, int times) {
-//       return null;
+//    public static void main(String[] args) {
+//       String str =" E::=    T     |		E		OP     T    ";
+//       String[] strs = str.split("::=");
+//       for(String s: strs){
+//          System.out.println(s);
+//       }
 //    }
-//    
-//    public String getSymbols() {
-//       return "";
-//    }
+
 
 }
