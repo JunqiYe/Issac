b17b5be22ff2c7590507558460ff781feb7231b5
Issac Ye <jy98@uw.edu>
2018-11-27 19:33:50 -0800

    new hw

---
+++ QuestionTree.java
@@ -0,0 +1,34 @@
+// Junqi Ye
+// CSE 143 With Sam Lee
+// HW 7
+// 
+import java.util.*;
+
+public class QuestionTree {
+   
+   public QuestionTree() {
+   
+   }
+   
+   
+   public void read(Scanner input) {
+   
+   }
+   
+   
+   public void write(PrintStream output) {
+   
+   }
+   
+   
+   public void askQuestions() {
+   
+   }
+   
+   
+   public boolean yesTo(String prompt) {
+   
+      return false;
+   }
+
+}
