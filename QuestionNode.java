5213e347f03cc1066ff20509a6c5154ed1aced3a
Issac PC <jy98@uw.edu>
2018-11-29 00:29:02 -0900

    comments

--- QuestionNode.java
+++ QuestionNode.java
@@ -1,7 +1,11 @@
 // Junqi Ye
 // CSE 143 With Sam Lee
 // HW 7
-// hi this is a test
+// This program simulates a tree node with two subbranches. This class is
+// encapsulated within the QuestionTree class which mean the clients will not
+// have access to this class. The node saves information regarding to the
+// object. It can be either a question node that has a yes branch and a no
+// branch, or a answer node with no branches.
 
 public class QuestionNode {
 
