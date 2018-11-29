0bc86ee50224f69dc4af5ab875b12b51da0a2165
Issac PC <jy98@uw.edu>
2018-11-29 00:09:01 -0900

    indent

--- QuestionNode.java
+++ QuestionNode.java
@@ -1,31 +1,31 @@
 // Junqi Ye
 // CSE 143 With Sam Lee
 // HW 7
-// 
+// hi this is a test
 
 public class QuestionNode {
 
-   public String data;
+    public String data;
    
-   public QuestionNode left;
+    public QuestionNode left;
    
-   public QuestionNode right;
+    public QuestionNode right;
 
-   public QuestionNode() {
+    public QuestionNode() {
       this("computer", null, null);
    }
    
-   public QuestionNode(String data) {
+    public QuestionNode(String data) {
       this(data, null, null);
    }
    
-   public QuestionNode(String data, QuestionNode left, QuestionNode right) {
-      this.data = data;
-      this.left = left;
-      this.right = right;
-   }
+    public QuestionNode(String data, QuestionNode left, QuestionNode right) {
+       this.data = data;
+       this.left = left;
+       this.right = right;
+    }
    
-   public boolean isAnswerNode() {
+    public boolean isAnswerNode() {
       return (left == null) && (right == null);
    }
 }
--- QuestionTree.java
+++ QuestionTree.java
@@ -10,126 +10,126 @@ import java.io.*;
 import java.util.*;
 
 public class QuestionTree {
+
+    private Scanner console;
+
+    private QuestionNode tree;
    
-   private Scanner console;
+    // post: creates a QestionTree object. If this is the first time inilizing
+    // the object, the first object that the program will guess is "computer".
+
+    public QuestionTree() {
+        console = new Scanner(System.in);
+        tree = new QuestionNode();
+    }
    
-   private QuestionNode tree;
-   
-   
-   // post: creates a QestionTree object. If this is the first time inilizing
-   // the object, the first object that the program will guess is "computer".
-   public QuestionTree() {
-      console = new Scanner(System.in);
-      tree = new QuestionNode();
-   }
-   
-   
-   // pre : takes a Scanner as input. The input should link to a file that 
-   // contains the information regarding the questions and answers. The file
-   // should be legal and in a standard format.
-   // post: read through the file and store the information. The information
-   // will later be used by the program to guess client's object.
-   public void read(Scanner input) {
-      tree = read(input, tree);
-   }
-   
-   
-   // public-private pair
-   // pre : takes a scanner input and a QuestionNode reference.
-   // post: The method will read through the input file and construct a 
-   // QuestionNode tree.
-   private QuestionNode read(Scanner input, QuestionNode node) {
-      QuestionNode tempNode = node;
-      if (input.hasNext()) {
-         String temp = input.nextLine();
-         if (temp.equals("Q:")) {
-            tempNode = new QuestionNode(input.nextLine());
-            tempNode.left = read(input, tempNode.left);
-            tempNode.right = read(input, tempNode.right);
-         } else {
-            tempNode = new QuestionNode(input.nextLine());
-         }
-      }
-      return tempNode;
-   }
+
+    // pre : takes a Scanner as input. The input should link to a file that
+    // contains the information regarding the questions and answers. The file
+    // should be legal and in a standard format.
+    // post: read through the file and store the information. The information
+    // will later be used by the program to guess client's object.
+    public void read(Scanner input) {
+        tree = read(input, tree);
+    }
    
    
-   // pre : takes PrintStream obejct as the parameter. The PrintStream should
-   // be open for writing.
-   // post: The method prints out and stores the the questions and answers in
-   // a file defined when creating the PrintStream object. Each individual
-   // output will be on separed lines. Each question will have a "Q:" in before
-   // it and each answer will have a "A:" on a separed line before it. The 
-   // newly added questions and answers will be updated to the original file.
-   public void write(PrintStream output) {
-      write(output, tree);
-   }
-   
-   
-   
-   // pre : takes a PrintStream object and a QuestionNode node.
-   // post: the method goes through the QuestionNode tree and print out the 
-   // questions and answers in pre-order. Each question and answer will have
-   // occupy exactly one line and there is a "Q:" and "A:" in front of the
-   // question and answer repectiverly. The "Q:" and "A:" will occupy one line
-   // as well.
-   private void write(PrintStream output, QuestionNode node) {
-      if (node != null)
-         if(node.isAnswerNode()) {
-            output.println("A:");
-            output.println(node.data);
-         } else {
-            output.println("Q:");
-            output.println(node.data);
-            write(output, node.left);
-            write(output, node.right);
-         }
-   }
-   
-   
-   // post: this method will askes the client questions about the object. The 
-   // client have to respond with "y" or "n" for the program to narrow down the
-   // the object and correctly guess the object. If the program fails to guess
-   // the object, the program will asks and store the object the client is  
-   // thinking and store describtion in a yes or no question. If it is the 
-   // first time guessing the object, the program will always guess "computer"
-   public void askQuestions() {
-      tree = askQuestions(tree);
-   }
-   
-   
-   
-   // public-private pair
-   // pre : takes a QuestionNode node.
-   // post: the method will go through the QuestionNode tree and ask the client
-   // questions on the tree. If the clients object is not on the tree, the 
-   // method will add the object and the yes-no describtion of the object into
-   // the tree.
-   private QuestionNode askQuestions(QuestionNode node){
-      if (node.isAnswerNode()) {
-         if (yesTo("Would your object happen to be " + node.data + "?")) {
-            System.out.println("Great, I got it right!");
-         } else {
-            System.out.print("What is the name of your object? ");
-            String object = console.nextLine().trim();
-            System.out.print("Please give me a yes/no question that\ndistinguishes " +
-                             "between your object\nand mine--> ");
-            String question = console.nextLine().trim();
-            if (yesTo("And what is the answer for your object?")) {
-               return new QuestionNode(question, new QuestionNode(object), node);
+    // public-private pair
+    // pre : takes a scanner input and a QuestionNode reference.
+    // post: The method will read through the input file and construct a
+    // QuestionNode tree.
+    private QuestionNode read(Scanner input, QuestionNode node) {
+        QuestionNode tempNode = node;
+        if (input.hasNext()) {
+            String temp = input.nextLine();
+            if (temp.equals("Q:")) {
+                tempNode = new QuestionNode(input.nextLine());
+                tempNode.left = read(input, tempNode.left);
+                tempNode.right = read(input, tempNode.right);
             } else {
-               return new QuestionNode(question, node, new QuestionNode(object));
+                tempNode = new QuestionNode(input.nextLine());
             }
-         }
-      } else {
-         if (yesTo(node.data)) {
-            node.left = askQuestions(node.left);
-         } else {
-            node.right = askQuestions(node.right);
-         }
-      }
-      return node;
-   }
+        }
+        return tempNode;
+    }
+   
+   
+    // pre : takes PrintStream obejct as the parameter. The PrintStream should
+    // be open for writing.
+    // post: The method prints out and stores the the questions and answers in
+    // a file defined when creating the PrintStream object. Each individual
+    // output will be on separed lines. Each question will have a "Q:" in before
+    // it and each answer will have a "A:" on a separed line before it. The
+    // newly added questions and answers will be updated to the original file.
+    public void write(PrintStream output) {
+       write(output, tree);
+    }
+   
+   
+   
+    // pre : takes a PrintStream object and a QuestionNode node.
+    // post: the method goes through the QuestionNode tree and print out the
+    // questions and answers in pre-order. Each question and answer will have
+    // occupy exactly one line and there is a "Q:" and "A:" in front of the
+    // question and answer repectiverly. The "Q:" and "A:" will occupy one line
+    // as well.
+    private void write(PrintStream output, QuestionNode node) {
+        if (node != null) {
+            if (node.isAnswerNode()) {
+                output.println("A:");
+                output.println(node.data);
+            } else {
+                output.println("Q:");
+                output.println(node.data);
+                write(output, node.left);
+                write(output, node.right);
+            }
+        }
+    }
+   
+
+    // post: this method will askes the client questions about the object. The
+    // client have to respond with "y" or "n" for the program to narrow down the
+    // the object and correctly guess the object. If the program fails to guess
+    // the object, the program will asks and store the object the client is
+    // thinking and store describtion in a yes or no question. If it is the
+    // first time guessing the object, the program will always guess "computer"
+    public void askQuestions() {
+        tree = askQuestions(tree);
+    }
+
+   
+    // public-private pair
+    // pre : takes a QuestionNode node.
+    // post: the method will go through the QuestionNode tree and ask the client
+    // questions on the tree. If the clients object is not on the tree, the
+    // method will add the object and the yes-no describtion of the object into
+    // the tree.
+    private QuestionNode askQuestions(QuestionNode node) {
+        if (node.isAnswerNode()) {
+            if (yesTo("Would your object happen to be " + node.data + "?")) {
+                System.out.println("Great, I got it right!");
+            } else {
+                System.out.print("What is the name of your object? ");
+                String object = console.nextLine().trim();
+                System.out.print("Please give me a yes/no question that\ndistinguishes " +
+                        "between your object\nand mine--> ");
+                String question = console.nextLine().trim();
+                if (yesTo("And what is the answer for your object?")) {
+                    return new QuestionNode(question, new QuestionNode(object), node);
+                } else {
+                    return new QuestionNode(question, node, new QuestionNode(object));
+                }
+            }
+        } else {
+            if (yesTo(node.data)) {
+                node.left = askQuestions(node.left);
+            } else {
+                node.right = askQuestions(node.right);
+            }
+        }
+        return node;
+    }
    
    
    
