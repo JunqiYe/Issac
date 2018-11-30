0839abfad18af467d347110f17d5f0584183b6e4
Issac PC <jy98@uw.edu>
2018-11-29 19:09:35 -0900

    comments

--- QuestionTree.java
+++ QuestionTree.java
@@ -3,9 +3,10 @@
 // HW 7
 // This program simulates a yes and no game. The client will have to think of 
 // an object and the program will asks the client series of question to find
-// the correct answer. If the program fails to find the corrent answer, the
-// program will learn from the user and become better and better at guessing
-// the answer.
+// the correct answer. If the program fails to find the correct answer, the
+// program will learn and record from the user and become better and better at
+// guessing the answer.
+
 import java.io.*;
 import java.util.*;
 
@@ -15,9 +16,9 @@ public class QuestionTree {
 
     private QuestionNode tree;
    
-    // post: creates a QestionTree object. If this is the first time initializing
-    // the object, the first object that the program will guess is "computer".
-
+    // post: creates a QuestionTree object. If this is the first time
+    // initializing the object, the first object that the program will guess
+    // is "computer".
     public QuestionTree() {
         console = new Scanner(System.in);
         tree = new QuestionNode("computer");
@@ -26,9 +27,10 @@ public class QuestionTree {
 
     // pre : takes a Scanner as input. The input should link to a file that
     // contains the information regarding the questions and answers. The file
-    // should be legal and in a standard format.
-    // post: read through the file and store the information. The information
-    // will later be used by the program to guess client's object.
+    // should be legal and organized in pre-order tree format.
+    // post: read through the file and store the information. Reading a new
+    // file will overwrite the previous information.The information will later
+    // be used by the program to guess client's object.
     public void read(Scanner input) {
         tree = read(input, tree);
     }
@@ -58,32 +60,31 @@ public class QuestionTree {
     // be open for writing.
     // post: The method prints out and stores the the questions and answers in
     // a file defined when creating the PrintStream object. Each individual
-    // output will be on separated lines. Each question will have a "Q:" in before
-    // it and each answer will have a "A:" on a separated line before it. The
-    // newly added questions and answers will be updated to the original file.
+    // output will be on separated lines. Each question will have a "Q:" on a
+    // separated line before it and each answer will have a "A:" on a
+    // separated line before it. The newly added questions and answers will
+    // be updated in file.
     public void write(PrintStream output) {
         write(output, tree);
     }
-   
-   
+
    
     // pre : takes a PrintStream object and a QuestionNode node.
-    // post: the method goes through the QuestionNode tree and print out the
-    // questions and answers in pre-order. Each question and answer will have
-    // occupy exactly one line and there is a "Q:" and "A:" in front of the
-    // question and answer respectively. The "Q:" and "A:" will occupy one line
-    // as well.
+    // post: the method goes through the QuestionNode tree in pre-order
+    // traversal and print out the questions and answers. Each question and
+    // answer will have occupy exactly one line and there is a "Q:" and "A:"
+    // in front of the question and answer respectively. The "Q:" and "A:"
+    // will occupy one line as well.
     private void write(PrintStream output, QuestionNode node) {
         if (node != null) {
             if (isAnswerNode(node)) {
                 output.println("A:");
-                output.println(node.data);
             } else {
                 output.println("Q:");
-                output.println(node.data);
-                write(output, node.left);
-                write(output, node.right);
             }
+            output.println(node.data);
+            write(output, node.left);
+            write(output, node.right);
         }
     }
    
@@ -103,7 +104,7 @@ public class QuestionTree {
     // pre : takes a QuestionNode node.
     // post: the method will go through the QuestionNode tree and ask the client
     // questions on the tree. If the clients object is not on the tree, the
-    // method will add the object and the yes-no describtion of the object into
+    // method will add the object and the yes-no description of the object into
     // the tree.
     private QuestionNode askQuestions(QuestionNode node) {
         if (isAnswerNode(node)) {
@@ -112,9 +113,9 @@ public class QuestionTree {
             } else {
                 System.out.print("What is the name of your object? ");
                 String object = console.nextLine().trim();
-                System.out.printf("Please give me a yes/no question " +
-                                  "that%ndistinguishes between your " +
-                                  "object%nand mine--> ");
+                System.out.println("Please give me a yes/no question that");
+                System.out.println("distinguishes between your object");
+                System.out.print("and mine--> ");
                 String question = console.nextLine().trim();
                 if (yesTo("And what is the answer for your object?")) {
                     return new QuestionNode(question, new QuestionNode(object), node);
@@ -133,12 +134,13 @@ public class QuestionTree {
     }
 
 
-    // post: the method returns true if the node is a answer node and false
+    // post: the method returns true if the node is an answer node and false
     // otherwise.
     private boolean isAnswerNode(QuestionNode node) {
-        return (node.left == null) && (node.right == null);
+        return node.left == null && node.right == null;
     }
-   
+
+
     // post: asks the user a question, forcing an answer of "y" or "n";
     //       returns true if the answer was yes, returns false otherwise
     public boolean yesTo(String prompt) {
@@ -151,6 +153,4 @@ public class QuestionTree {
         }
         return response.equals("y");
     }
-
-
 }
